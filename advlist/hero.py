c657c3ea29f36d05d09968f3f881cff7fdeedcc7
Bikash Bhaila <bikashbhaila@gmail.com>
2021-08-02 22:41:05 +0000

    list and dictionaries practices

---
+++ advlist/hero.py
@@ -0,0 +1,3 @@
+hero = {'name': 'Spider-Man', 'real name': 'Peter Parker', 'age': 17, 'powers': ['spidey sense', 'super strength', "mosquito catchin'", 'agility'], 'weaknesses': ['Mary Jane', 'Uncle Ben', 'symbiote']}
+
+print(hero["powers"][2])
---
+++ listmethods/listmeth03.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+names = ["John Doe", "Jane Doe", "Abc Xyz"]
+
