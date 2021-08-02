af01ac1d03278c164932beefb4d2fceb81a7a466
Bikash Bhaila <bikashbhaila@gmail.com>
2021-08-02 22:00:36 +0000

    list features

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+animal_list = ["Fox", "Fly", "ant"]
+animal_list2 = ["dog", "cat", "cow"]
+
+# animal_list.extend(animal_list2)
+print(animal_list)
+
+animal_list.append(animal_list2)
+
+print(animal_list)
+
+print(animal_list[3][-1])
---
+++ advlist/icecream_list.py
@@ -0,0 +1,13 @@
+icecream= ["flavors", "salty"] 
+
+tlgclass= ["Adrian","Bikash","Chas","Chathula","Chris","Hongyi","Jauric","Joe L.","Joe V.","Josh","Justin","Karlo","Kerri-Leigh","Jason","Nicholas","Peng","Philippe","Pierre","Stephen","Yun"]
+
+icecream.append(int(99))
+print(icecream)
+
+user_input = int(input("Enter a number between 0 and 19"))
+
+
+print(icecream[2], icecream [1], ", and", tlgclass[user_input], "chooses to be", icecream[1])
+
+
